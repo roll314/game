31699af021e78f1e3f11ffb1df864e764e4a9d66
Peter Kornuishin <pkornyushin@ptsecurity.com>
2019-08-04 00:14:15 +0300

    combined into the root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,41 @@
 import {Component, OnInit} from '@angular/core';
+import {ComponentWithSubscription} from './utils/ComponentWithSubscription';
+import {GameFacade} from './store/facade';
+import {GameStatus} from './store/reducer';
 
+const INFO_FONT_SIZE_FACTOR = 20;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // tslint:disable-next-line:no-host-metadata-property
+  host: {
+    '(window:resize)': 'sizeToFactor()'
+  }
 })
-export class AppComponent implements OnInit {
-  title = 'game';
+export class AppComponent extends ComponentWithSubscription implements OnInit {
+  score: number;
+  timeToColorChange: number;
+
+  infoFontSize: number;
+  gameStatus: GameStatus;
+  GameStatus = GameStatus;
+
+  constructor(private gameFacade: GameFacade) {
+    super();
+  }
 
   ngOnInit(): void {
+    this.sizeToFactor();
 
+    this.subscribeUntilDestroyed(this.gameFacade.getGameStatus(), (status: GameStatus) => this.gameStatus = status);
+    this.subscribeUntilDestroyed(this.gameFacade.getScore(), (score: number) => this.score = score);
+    // tslint:disable-next-line:max-line-length
+    this.subscribeUntilDestroyed(this.gameFacade.getTimeToColorChange(), (timeToColorChange: number) => this.timeToColorChange = timeToColorChange);
+  }
+
+  private sizeToFactor() {
+    this.infoFontSize = Math.min(window.innerHeight, window.innerWidth) / INFO_FONT_SIZE_FACTOR;
   }
 }
